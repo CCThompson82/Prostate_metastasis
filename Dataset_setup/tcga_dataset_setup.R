441357a9c82a4909f7f1d28a1bdfea018c751b2a
Christopher Thompson <christophercthompson82@gmail.com>
2016-05-24 15:04:50 +0100

    Debug row.names being dropped by write_feather

--- Dataset_setup/tcga_dataset_setup.R
+++ Dataset_setup/tcga_dataset_setup.R
@@ -1,15 +1,23 @@
 # This script utilizes the TCGA2STAT package for convenient access to the TCGA 
 # portal for dataset initiation.
+
+#package import
 library(TCGA2STAT)
+library(feather)
+
 # Download datasets into a list called 'PRAD'
 PRAD <- getTCGA(disease = "PRAD", 
                 data.type = "RNASeq2",
                 p = getOption("mc.cores", 2L), 
                 clinical = T)
-data.frame(t(PRAD[[1]])) -> gc #Gene counts 
-data.frame(PRAD[[2]]) -> clinical
+
+#wrangle into data_frame
+data.frame(t(PRAD[[1]]), stringsAsFactors = FALSE) -> gc #Gene counts 
+gc <- cbind("gc_index" = row.names(gc), gc)
+data.frame(PRAD[[2]], stringsAsFactors = FALSE) -> clinical
+clinical <- cbind("clinical_index" = row.names(clinical), clinical) #move row names to first column
+gc$gc_index <- sapply(gc$gc_index, substr, 0, 12) #shorten extended ID to the standard 12 characters observed in clininical data set
+
 #write files to be picked up in python
-library(feather)
 write_feather(gc, "Gene_counts.feather") 
 write_feather(clinical, "Clinical_data.feather")
-##Need to fix so that row names are not dumped during feather save.
