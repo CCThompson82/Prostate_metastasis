208f1e5b0423c12f6a464fd0a6c748249e85cab4
Christopher Thompson <christophercthompson82@gmail.com>
2016-05-25 11:12:42 +0100

    Debug feather file import into python shell

--- Dataset_cleanup/clean_up.py
+++ Dataset_cleanup/clean_up.py
@@ -4,8 +4,20 @@ Created on Tue May 24 17:13:54 2016
 
 @author: ccthomps
 """
-#%%
 import feather
-#%%
-clinical = feather.read_dataframe('Clinical_data.feather')
+import numpy as np
 
+#Import feather files that were downloaded and saved using TCGA2STAT package in R:
+clinical = feather.read_dataframe('Clinical_data.feather')
+gene_counts = feather.read_dataframe('Gene_counts.feather')
+
+#Check data import:
+if np.isfinite(clinical.shape[0]) :
+    print("Clinical data set imported")
+else :
+    print("Error in Clinical data set import")
+
+if np.isfinite(gene_counts.shape[0]) :
+    print("Gene Counts data set imported")
+else :
+    print("Error in Gene_counts import")
