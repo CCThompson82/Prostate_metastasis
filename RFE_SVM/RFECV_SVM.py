f12724c1061b88810d0e98cd644f69329ddbbf47
Christopher Thompson <christophercthompson82@gmail.com>
2016-06-03 12:59:23 +0100

    Minor comment changes to RFECV SVM script

--- RFE_SVM/RFECV_SVM.py
+++ RFE_SVM/RFECV_SVM.py
@@ -1,17 +1,6 @@
 from sklearn.feature_selection import RFECV
 from sklearn.svm import SVC
-from sklearn.metrics import f1_score, make_scorer
-import feather
 
-#X_train = feather.read_dataframe("X_train.feather") #make files accessible to individual py programs
-#y_train = feather.read_dataframe("y_train.feather") #make files accessible to individual py programs
-
-f1_scorer = make_scorer(f1_score,
-                        greater_is_better = True,
-                        needs_proba= False,
-                        needs_threshold=False,
-                        pos_label='n1',
-                        average = 'binary')
 estimator = SVC(C=.5,
                 kernel='linear',
                 probability=False,
@@ -27,7 +16,6 @@ selector = RFECV(estimator,
                 scoring = f1_scorer)
 selector.fit(X_train, y_train)
 
-from sklearn.metrics import classification_report
 print(classification_report(y_train,
                             selector.predict(X_train),
                             target_names = ['n0', 'n1']))
