c93c361b084954add774c608de28f9346be7629c
Christopher Thompson <christophercthompson82@gmail.com>
2016-06-03 10:52:56 +0100

    Move AdaBoost Model to its own script and run in IPython

---
+++ AdaBoost/adaboost.py
@@ -0,0 +1,38 @@
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+base_est = DecisionTreeClassifier(criterion='gini',
+                                  splitter='best',
+                                  max_depth=2,
+                                  min_samples_split=2,
+                                  min_samples_leaf=1,
+                                  min_weight_fraction_leaf=0.0,
+                                  max_features=None,
+                                  random_state=123,
+                                  max_leaf_nodes=None,
+                                  class_weight={'n0':0.15, 'n1':0.85},
+                                  presort=False)
+
+estimator = AdaBoostClassifier(base_estimator=base_est,
+                         n_estimators=100,
+                         learning_rate=.001,
+                         algorithm='SAMME',
+                         random_state=123)
+
+clf = GridSearchCV(estimator,
+                   param_grid = {},
+                   scoring=f1_scorer,
+                   fit_params=None,
+                   n_jobs=1,
+                   iid=True,
+                   refit=True,
+                   cv= 5,
+                   verbose=0,
+                   pre_dispatch='2*n_jobs',
+                   error_score='raise')
+clf.fit(X_train, y_train)
+
+ada_clf = clf.best_estimator_
+print(classification_report(y_train,
+                            clf.predict(X_train),
+                            target_names = ['n0', 'n1']))
