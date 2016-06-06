b47c9fcca88c9cc47cca407f8f785d1da5fbd452
Christopher Thompson <christophercthompson82@gmail.com>
2016-06-06 11:34:40 +0100

    Edit SVM classification to train on the pca transformed DF

--- RFE_SVM/RFECV_SVM.py
+++ RFE_SVM/RFECV_SVM.py
@@ -6,7 +6,7 @@ estimator = SVC(C=.5,
                 probability=False,
                 tol=0.001,
                 cache_size=200,
-                class_weight={'n0':0.05, 'n1':0.95},
+                class_weight='balanced',
                 verbose=False,
                 max_iter=-1,
                 random_state= 123)
@@ -14,12 +14,12 @@ selector = RFECV(estimator,
                  step = 2,
                  cv = 5,
                 scoring = f1_scorer)
-selector.fit(X_train, y_train)
+selector.fit(Xpca_train, ypca_train)
 
-print(classification_report(y_train,
-                            selector.predict(X_train),
+print(classification_report(ypca_train,
+                            selector.predict(Xpca_train),
                             target_names = ['n0', 'n1']))
-print(X_train.columns[selector.support_])
+print(Xpca_train.columns[selector.support_])
 SVM_clf = selector.estimator_
 
 print(SVM_clf)
