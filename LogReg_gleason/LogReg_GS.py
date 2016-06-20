2b62ee3a1d130457ae14232478f4fb596144162f
Christopher Thompson <christophercthompson82@gmail.com>
2016-06-20 10:55:30 +0100

    LogReg with Gleason and LDA_decision_function

--- LogReg_gleason/LogReg_GS.py
+++ LogReg_gleason/LogReg_GS.py
@@ -1,31 +1,30 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import SGDClassifier
+gleason = clinical['gleasonscore']
+gleason = gleason.loc[y.index]
+
 lda = LinearDiscriminantAnalysis(solver='svd',
                                  shrinkage=None,
                                  priors=None,
                                  n_components=1,
                                  store_covariance=False,
                                  tol=0.0001)
-#lda.fit(X_k.loc[X_train.index],y_train)
-#Xlda_train = lda.transform(X_k.loc[X_train.index])
-#Xlda_test = lda.transform(X_k.loc[X_test.index])
 
-gleason = clinical['gleasonscore']
-gleason = gleason.loc[y.index]
-gleason_train = gleason.loc[y_train.index]
-gleason_test = gleason.loc[y_test.index]
-gleason_train = gleason_train.reshape(-1,1)
-gleason_test = gleason_test.reshape(-1,1)
 
-logisticDF = pd.DataFrame({#'lda_transform' : Xlda_train[:,0],
-                           'gleason_scores' : gleason_train[:,0]}, index = y_train.index)
-logisticDF_test = pd.DataFrame({#'lda_transform' : Xlda_test[:,0],
-                                'gleason_scores' : gleason_test[:,0]}, index = y_test.index)
+lda.fit(X_k.loc[X_train.index],y_train)
+
+logisticDF = pd.DataFrame({'lda_decision': lda.decision_function(X_k),
+                           'gleason': gleason}, index=X_k.index)
+
+logisticDF_train = logisticDF.loc[X_train.index, :]
+logisticDF_test = logisticDF.loc[X_test.index, :]
+
 
 estimator = LogisticRegression(penalty='l2',
                               dual=False,
                               tol=0.0001,
-                              C=.1,
+                              C=1,
                               fit_intercept=True,
                               intercept_scaling=1,
                               class_weight='balanced',
@@ -36,8 +35,9 @@ estimator = LogisticRegression(penalty='l2',
                               verbose=0,
                               warm_start=False,
                               n_jobs=1)
+
 clf = GridSearchCV(estimator,
-                   param_grid = {},
+                   param_grid = {'C': [1,0.1, 0.01, 0.001, 0.0001]},
                    scoring=fbeta_scorer,
                    fit_params=None,
                    n_jobs=1,
@@ -47,11 +47,11 @@ clf = GridSearchCV(estimator,
                    verbose=0,
                    pre_dispatch='2*n_jobs',
                    error_score='raise')
-clf.fit(logisticDF, y_train)
+clf.fit(logisticDF_train, y_train)
 LR_clf = clf.best_estimator_
 print(LR_clf)
 print(classification_report(y_train,
-                            LR_clf.predict(logisticDF),
+                            LR_clf.predict(logisticDF_train),
                             target_names = ['n0', 'n1']))
-print('\nF beta: ', fbeta_score(y_train, LR_clf.predict(logisticDF), beta = 2, pos_label='n1'))
-print('\nMCC: ',matthews_corrcoef(y_train, LR_clf.predict(logisticDF)))
+print('\nF beta: ', fbeta_score(y_train, LR_clf.predict(logisticDF_train), beta = 2, pos_label='n1'))
+print('\nMCC: ',matthews_corrcoef(y_train, LR_clf.predict(logisticDF_train)))
