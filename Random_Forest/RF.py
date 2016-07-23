e6f64ed4586d93cba648aa322b2faa7f5013b094
Christopher Thompson <christophercthompson82@gmail.com>
2016-07-23 10:18:54 +0100

    Change structure of project

--- Random_Forest/RF.py
+++ Random_Forest/RF.py
@@ -1,12 +1,14 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.learning_curve import learning_curve
+from sklearn.learning_curve import validation_curve
 
-estimator = RandomForestClassifier(n_estimators=300,
-                                   criterion='entropy',
-                                   max_depth=3,
-                                   min_samples_split=40,
+estimator = RandomForestClassifier(n_estimators=500,
+                                   criterion='gini',
+                                   max_depth=None,
+                                   min_samples_split=50,
                                    min_samples_leaf=5,
                                    min_weight_fraction_leaf=0.0,
-                                   max_features= 0.05,
+                                   max_features= 'auto',
                                    max_leaf_nodes=None,
                                    bootstrap=True,
                                    oob_score=False,
@@ -18,17 +20,17 @@ estimator = RandomForestClassifier(n_estimators=300,
 
 
 train_sizes, train_scores, test_scores = learning_curve(estimator,
-                                                        X_k.loc[X_train.index, :],
+                                                        X_train,
                                                         y_train,
                                                         train_sizes = [0.33, 0.66, 1],
                                                         cv = 4,
                                                         scoring=fbeta_scorer,
                                                         exploit_incremental_learning= False)
-param_range = [10, 50, 100, 250, 500]
+param_range = [1, 2, 3, 4, 5, 6]
 train_scores_val, val_scores_val = validation_curve(estimator,
-                                                    X_k.loc[X_train.index, :],
+                                                    X_train,
                                                     y_train,
-                                                    param_name = "n_estimators",
+                                                    param_name = "max_depth",
                                                     param_range= param_range,
                                                     cv=4,
                                                     scoring = fbeta_scorer,
